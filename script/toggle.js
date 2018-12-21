310fd6613fa021d62978357abdb9760d502d7f54
OnePunchDev <45498211+OnePunchDev@users.noreply.github.com>
2018-12-21 14:37:43 +0100

    Changed script and images
    Better functions

--- script/toggle.js
+++ script/toggle.js
@@ -1,43 +1,77 @@
+
 window.addEventListener("DOMContentLoaded", function(event) {
-  document.getElementById("showall").addEventListener("click", showall);
-  document.getElementById("togglefireextinguisher").addEventListener("click", togglefireextinguisher);
-  document.getElementById("toggledesks").addEventListener("click", toggledesks);
-  document.getElementById("togglebrooms").addEventListener("click", togglebrooms);
+  var toggleallcbox = document.getElementById("toggleall")
+  var redcirclecbox = document.getElementById("showredcircle")
+  var greensquarecbox = document.getElementById("showgreensquare")
+  var purpletrianglecbox = document.getElementById("showpurpletriangle")
+  toggleallcbox.addEventListener("click", toggleall);
+  redcirclecbox.addEventListener("change", function(){show("redcircle", redcirclecbox)});
+  greensquarecbox.addEventListener("change", function(){show("greensquare", greensquarecbox)});
+  purpletrianglecbox.addEventListener("change", function(){show("purpletriangle", purpletrianglecbox)});
 });
 
+var hidden = false;
+
+function toggleall() {
+  if (hidden == false) {
+    hideall();
+    hidden = true;
+  } else {
+    showall();
+    hidden = false;
+  }
+}
+
+function hideall() {
+  var mapitems = document.getElementById("mapitems")
+  var imagelist = mapitems.querySelectorAll('img')
+  for (let i = 0; i < imagelist.length ; ++i) {
+    imagelist[i].style.display = "none";
+  }
+  uncheck();
+}
+
 function showall() {
-  var x = document.getElementById("fireextinguisher");
-  var y = document.getElementById("desks");
-  var z = document.getElementById("brooms");
-  x.style.display = "block";
-  y.style.display = "block";
-  z.style.display = "block";
+  var mapitems = document.getElementById("mapitems")
+  var imagelist = mapitems.querySelectorAll('img')
+  for (let i = 0; i < imagelist.length ; ++i) {
+    imagelist[i].style.display = "block";
+  }
+  check();
 }
 
-
-function togglefireextinguisher() {
-  var x = document.getElementById("fireextinguisher");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
+function uncheck() {
+  var filterbar = document.getElementById("filterbar")
+  var checkboxlist = filterbar.querySelectorAll('input')
+  for (let i = 0; i < checkboxlist.length ; ++i) {
+      checkboxlist[i].checked = false;
   }
 }
 
-function toggledesks() {
-  var x = document.getElementById("desks");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
+function check() {
+  var filterbar = document.getElementById("filterbar")
+  var checkboxlist = filterbar.querySelectorAll('input')
+  for (let i = 0; i < checkboxlist.length ; ++i) {
+      checkboxlist[i].checked = true;
   }
 }
 
-function togglebrooms() {
-  var x = document.getElementById("brooms");
-  if (x.style.display === "none") {
+function show(image, checkbox) {
+  var x = document.getElementById(image)
+  if (ischecked(checkbox) === true) {
     x.style.display = "block";
+    hidden = false;
   } else {
     x.style.display = "none";
+    hidden = true;
   }
 }
+
+function ischecked(checkbox) {
+  if (checkbox.checked === true) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
